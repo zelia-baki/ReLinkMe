d4c022c8468ddfad65174bf695132fc4923bb424
zelia-baki <rakotomalala.babazhelia@gmail.com>
2025-11-11 21:56:21 +0300

    initialisation du projet avec React JS et partie recruteur deja init

--- relinkme_backend/backend/urls.py
+++ relinkme_backend/backend/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/core/', include('core.urls')),
+    path('api/recruteur/', include('recruteur.urls')),
 ]
 

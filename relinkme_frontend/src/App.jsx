e412ee2dd969626c745d1f4148a57c5d978628cb
ci-kandy <ravelomalalakcindy@gmail.com>
2025-11-17 06:57:17 +0300

    MAJ de package.json

--- relinkme_frontend/src/App.jsx
+++ relinkme_frontend/src/App.jsx
@@ -4,6 +4,8 @@ import CompetencesPage from "@/modules/chomeurs/pages/CompetencesPage";
 import ExploitsPage from "@/modules/chomeurs/pages/ExploitsPage";
 import InscriptionChomeur from "@/modules/chomeurs/pages/InscriptionChomeur";
 import ConnexionPage from "./modules/core/pages/ConnexionCore";
+import Admin from "./modules/admin/pages/Admin";
+import Demande from "./modules/admin/pages/Demande";
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/chomeur/exploits" element={<ExploitsPage />} />
         <Route path="/chomeur/inscriptions" element={<InscriptionChomeur />} />
         <Route path="/connexion" element={<ConnexionPage />} />
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="admin/demande" element={<Demande/>}/>
       </Routes>
     </BrowserRouter>
   );

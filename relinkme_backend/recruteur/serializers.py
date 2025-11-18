8420441e3d8716df7d4a124e7d80faf2e0b7a8b4
zelia-baki <rakotomalala.babazhelia@gmail.com>
2025-11-18 08:55:24 +0300

    offre et recruteur OK

--- relinkme_backend/recruteur/serializers.py
+++ relinkme_backend/recruteur/serializers.py
@@ -118,18 +118,28 @@ class RecruteurSerializer(serializers.ModelSerializer):
 
 
 class OffreSerializer(serializers.ModelSerializer):
+    """
+    Serializer pour les offres d'emploi.
+    Le champ 'recruteur' est géré automatiquement par perform_create() dans la vue.
+    """
+    # Champs en lecture seule pour affichage
     recruteur_nom = serializers.CharField(source='recruteur.nom_entreprise', read_only=True)
+    recruteur_id = serializers.IntegerField(source='recruteur.id', read_only=True)
+    
+    # 👇 AJOUT IMPORTANT : recruteur doit être en lecture seule
+    recruteur = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Offre
         fields = [
-            'id', 'code_offre', 'recruteur', 'recruteur_nom',
+            'id', 'code_offre', 'recruteur', 'recruteur_id', 'recruteur_nom',
             'titre', 'description', 'type_contrat', 'salaire',
             'date_limite', 'statut', 'date_creation'
         ]
-        read_only_fields = ('id', 'code_offre', 'date_creation', 'recruteur_nom')
-
-
+        read_only_fields = (
+            'id', 'code_offre', 'date_creation', 
+            'recruteur', 'recruteur_id', 'recruteur_nom'  # 👈 recruteur ajouté ici
+        )
 class OffreCompetenceSerializer(serializers.ModelSerializer):
     offre_titre = serializers.CharField(source='offre.titre', read_only=True)
     competence_nom = serializers.CharField(source='competence.libelle', read_only=True)
